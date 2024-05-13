ece437c1c6f06a454c1908314abff0763f94c1b9
SachinBishnoi-503 <bishnoisachin626@gmail.com>
2024-05-13 11:39:34 +0530

    slider section6 completed

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,3 +3,32 @@ let menulist = document.querySelector(".nav-items");
 menuicon.addEventListener("click", function () {
     menulist.classList.toggle("show");
 })
+
+$(".parentslider").slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    autoplay: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    arrows: false,
+    responsive: [
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots: true
+            }
+        }
+    ]
+});
